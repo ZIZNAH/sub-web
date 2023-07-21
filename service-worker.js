cfb5dfcf40a2d9acb4fc09fc29e18c1e3e19dc28
ZIZNAH <291301440@qq.com>
2023-07-21 13:56:58 +0800

    add customBackend

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/js/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/js/workbox-v4.3.1"});
 
 importScripts(
-  "/js/precache-manifest.313789c0cf33305cca9b8e3a4fb0f76e.js"
+  "/js/precache-manifest.eeddce9c219730f5864d40c22944ca2d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "sub-web"});
